b795f41dbe5075d9fbaf4795fb615e980a461b95
André Storhaug <andr3.storhaug@gmail.com>
2025-06-01 19:14:09 +0200

    Upate evaluation script

--- evaluation/methods2test_runnable/evaluate_tests.py
+++ evaluation/methods2test_runnable/evaluate_tests.py
@@ -15,11 +15,21 @@ logger = logging.getLogger(__name__)
 
 SCRIPT_PATH: str = Path(os.path.abspath(__file__))
 SCRIPT_DIR: Path = SCRIPT_PATH.parent
-DATA_DIR = SCRIPT_DIR.parents[1] / "data" / "methods2test_runnable" / "fixed"
-SAVE_DIR = SCRIPT_DIR.parents[1] / "data" / "methods2test_runnable" / "coverage"
+
 
 
 os.environ["GIT_TERMINAL_PROMPT"] = "0"  # Disable git terminal prompt
+
+def parse_args():
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser(description="Evaluate generated tests on methods2test dataset.")
+    parser.add_argument("--split", type=str, default="test", help="Dataset split")
+    parser.add_argument("--data_dir", type=str, default="../../data/methods2test_runnable/fixed", help="Temporary directory for caching and repos. Relative to script directory.")
+    parser.add_argument("--output_dir", type=str, default="../../data/methods2test_runnable/coverage", help="Directory to store results. Relative to script directory.")
+    parser.add_argument("--num_proc", type=int, default=1, help="Number of parallel threads")
+    parser.add_argument("--tmp_dir", type=str, default=".tmp", help="Temporary directory for caching and repos. Relative to script directory.")
+
+    return parser.parse_args()
 
 
 def trim_end_brac(input_string):
@@ -74,13 +84,13 @@ def jsonl_writer_worker(writeq: Queue):
         except queue.Empty:
             continue  
     
-def process_test(dataq: Queue, writeq: Queue, progressq: Queue, sem: BoundedSemaphore):
+def process_test(dataq: Queue, writeq: Queue, progressq: Queue, sem: BoundedSemaphore, tmp_dir):
     logger.info("Starting worker process")
-    meta_ds = load_dataset("andstor/methods2test_meta", "golden_commit", split="test", cache_dir=".tmp/cache")
+    meta_ds = load_dataset("andstor/methods2test_meta", "golden_commit", split="test", cache_dir=tmp_dir / "cache")
     logger.info("Loaded meta dataset")
     meta_ds_df = meta_ds.to_pandas().set_index("id")
     logger.info("Converted meta dataset to DataFrame")
-    raw_ds = load_dataset("andstor/methods2test_raw", split="test", cache_dir=".tmp/cache")
+    raw_ds = load_dataset("andstor/methods2test_raw", split="test", cache_dir=tmp_dir / "cache")
     logger.info("Loaded raw dataset")
     raw_ds_df = raw_ds.to_pandas().set_index("id")
     logger.info("Converted raw dataset to DataFrame")
@@ -94,7 +104,7 @@ def process_test(dataq: Queue, writeq: Queue, progressq: Queue, sem: BoundedSema
             if item is None:
                 logger.info("No more items to process, exiting worker")
                 break
-            test_ids, inc_gen_ids, file_path, output_file = item
+            test_ids, file_path, output_file = item
         except queue.Empty:
             logger.info("No more items to process, exiting worker")
             continue
@@ -118,7 +128,7 @@ def process_test(dataq: Queue, writeq: Queue, progressq: Queue, sem: BoundedSema
         repo_url = sample_raw[ "repository"]["url"]
         repo_name = repo_url.split('/')[-1]
 
-        local_dir = Path(".tmp") / "repos" / str(os.getpid())
+        local_dir = tmp_dir / "repos" / str(os.getpid())
         os.makedirs(local_dir, exist_ok=True)
         repo_path = local_dir / repo_name
             
@@ -162,55 +172,6 @@ def process_test(dataq: Queue, writeq: Queue, progressq: Queue, sem: BoundedSema
                     executor.register_unit_test(testCandidate)
                     executor.register_focal_method(focal_method)
                     executor.install_coverage_tool()
-                    if id not in inc_gen_ids:
-                        # First try to compile the original code
-                        #executor.clean()
-                        out, err, returncode = executor.execute()
-                        logger.debug(out)
-                        logger.debug(err)
-                        logger.debug(returncode)
-                        
-                        
-                        results = executor.get_results()
-                        status = None
-                        if results is None:
-                            logger.info("BUILD ERROR")
-                            status = "build error"
-                        elif results["skipped"] > 0:
-                            logger.info("TEST SKIPPED")
-                            status = "skipped"
-                        elif results["failures"] > 0:
-                            logger.info("TEST FAILED")
-                            status = "failed"
-                        elif results["errors"] > 0:
-                            logger.info("TEST ERROR")
-                            status = "error"
-                        else:
-                            status = "success"
-                            logger.info("TEST PASSED")
-                        
-                        
-                        coverage = None
-                        if results is not None:
-                            coverage = executor.get_coverage_report()
-                            logger.info(pd.Series(coverage).to_frame().T)
-                        data = {}
-                        data["id"] = id
-                        data["status"] = status
-                        data["generated"] = 0
-                        # add coverage to data without knowing the keys
-                        if coverage is not None:
-                            data.update(coverage)
-                        writeq.put((data, output_file))
-                        
-                        if status == "build error":
-                            logger.info("Build error in original code")
-                            continue
-                        if status == "skipped":
-                            logger.info("Test skipped in original code")
-                            continue
-                    
-                    
                     
                     ### GENERATED CODE ###
                     # Now try to compile the generated code
@@ -248,10 +209,11 @@ def process_test(dataq: Queue, writeq: Queue, progressq: Queue, sem: BoundedSema
                     if results is not None:
                         coverage = executor.get_coverage_report()
                         logger.info(pd.Series(coverage).to_frame().T)
+                        
                     data = {}
                     data["id"] = id
                     data["status"] = status
-                    data["generated"] = 1
+                    data["build_tool"] = executor.build_system.get_name()
                     # add coverage to data without knowing the keys
                     if coverage is not None:
                         data.update(coverage)
@@ -262,6 +224,7 @@ def process_test(dataq: Queue, writeq: Queue, progressq: Queue, sem: BoundedSema
                     data = {}
                     data["id"] = id
                     data["status"] = "exception"
+                    data["build_tool"] = executor.build_system.get_name() if executor.build_system else None
                     with open(output_file, "a") as exception_file:
                         exception_file.write(json.dumps(data) + "\n")
                 finally:
@@ -273,6 +236,7 @@ def process_test(dataq: Queue, writeq: Queue, progressq: Queue, sem: BoundedSema
                 data = {}
                 data["id"] = id
                 data["status"] = "exception"
+                data["build_tool"] = None
                 with open(output_file, "a") as exception_file:
                     exception_file.write(json.dumps(data) + "\n")
         finally:
@@ -288,13 +252,10 @@ def process_test(dataq: Queue, writeq: Queue, progressq: Queue, sem: BoundedSema
             except BaseException:
                 pass
 
-        
-        
-    
 
-def find_file_paths():
+def find_file_paths(data_dir):
     file_paths = []
-    for root, dirs, files in os.walk(DATA_DIR):
+    for root, dirs, files in os.walk(data_dir):
         for file in files:
             if file.endswith(".jsonl"):
                 file_path = os.path.join(root, file)
@@ -304,9 +265,11 @@ def find_file_paths():
     
 def main(args):
     
-    num_proc = args.num_proc
+    tmp_dir = SCRIPT_DIR / args.tmp_dir
+    save_dir = SCRIPT_DIR / args.output_dir
+    data_dir = SCRIPT_DIR / args.data_dir
     
-    file_paths = find_file_paths()
+    file_paths = find_file_paths(data_dir)
     total_tests = sum([sum(1 for line in open(filename)) for filename in file_paths])
     logger.info(f"Total number of test cases: {total_tests}")
     
@@ -323,11 +286,11 @@ def main(args):
     jsonl_writer.start()
     
     
-    sem = BoundedSemaphore(num_proc)
+    sem = BoundedSemaphore(args.num_proc)
     dataq = Queue()
     process_test_workers = [
-        Process(target=process_test, args=(dataq, writeq, progressq, sem), daemon=True)
-        for _ in range(num_proc)
+        Process(target=process_test, args=(dataq, writeq, progressq, sem, tmp_dir), daemon=True)
+        for _ in range(args.num_proc)
     ]
     for p in process_test_workers:
         p.start()
@@ -336,7 +299,7 @@ def main(args):
     
     
     for file_path in file_paths:
-        res_file_dir = SAVE_DIR / Path(*file_path.split(os.sep)[-4:-1]) # Extract method, namespace, and model name from the path
+        res_file_dir = save_dir / Path(*file_path.split(os.sep)[-4:-1]) # Extract method, namespace, and model name from the path
         os.makedirs(res_file_dir, exist_ok=True)
         output_file = res_file_dir / "jacoco.jsonl"
         
@@ -352,18 +315,11 @@ def main(args):
         
         # Resume logic: collect already processed ids
         processed_ids = set()
-        incomplete_ids = set()
         if os.path.exists(output_file):
             out_df = pd.read_json(output_file, orient='records', lines=True, dtype=False)
             if not out_df.empty:
                 out_df = out_df.set_index("id")
-                # Drop all entries with duplicated ids (keep only unique ids)
-                out_df_valid = out_df[~out_df.index.duplicated(keep=False)]
-                # Drop entries with status in ["exception", "skipped", "build error"]
-                out_df_valid = out_df_valid[~out_df_valid["status"].isin(["exception", "skipped", "build error"])]
-                incomplete_ids.update(out_df_valid.index)
-                # remove incomplete ids from processed_ids
-                processed_ids.update(set(out_df.index) - incomplete_ids)
+                processed_ids.update(set(out_df.index))
         progressq.put(len(processed_ids))
         
         gen_ds_df["repo_id"] = gen_ds_df["id"].str.split("_").str[0]
@@ -372,11 +328,11 @@ def main(args):
         
         for repo_id in constrained_iterator(sem, repo_ids):
             test_ids = set(gen_ds_df.loc[[repo_id]]["id"].tolist())
+            
             if not test_ids:
                 logger.warning(f"No test cases found for repo_id: {repo_id}")
                 sem.release()
                 continue
-            inc_gen_ids = test_ids.intersection(incomplete_ids)
             test_ids = test_ids - processed_ids
             
             if len(test_ids) == 0:
@@ -386,11 +342,10 @@ def main(args):
             
             # Sort test_ids to ensure consistent order
             test_ids = sorted(test_ids, key=lambda s: tuple(map(int, s.split('_'))))
-            inc_gen_ids = sorted(list(inc_gen_ids), key=lambda s: tuple(map(int, s.split('_'))))
-            dataq.put((test_ids, inc_gen_ids, file_path, output_file))
+            dataq.put((test_ids, file_path, output_file))
 
         
-    for _ in range(num_proc):
+    for _ in range(args.num_proc):
         dataq.put(None)
     for p in process_test_workers:
         p.join()
@@ -411,8 +366,6 @@ if __name__ == "__main__":
         datefmt="%H:%M:%S",
     )
     
-    parser = argparse.ArgumentParser(description="Evaluate tests with optional multiprocessing.")
-    parser.add_argument("--num_proc", type=int, default=1, help="Number of processes to use (default: 1)")
-    args = parser.parse_args()
+    args = parse_args()
     
     main(args)
