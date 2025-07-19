47a5c52a4ae6441b8348ea65f2297fb4d52cd4c5
André Storhaug <andr3.storhaug@gmail.com>
2025-07-19 03:39:25 +0200

    Refactor

--- evaluation/methods2test_runnable/validate_runnable.py
+++ evaluation/methods2test_runnable/validate_runnable.py
@@ -23,7 +23,7 @@ def parse_args():
     """Parse command line arguments."""
     parser = argparse.ArgumentParser(description="Validate runnable test cases.")
     parser.add_argument("--split", type=str, default="test", help="Dataset split")
-    parser.add_argument("--output_dir", type=str, default="../../data/methods2test_runnable/coverage", help="Directory to store results. Relative to script directory.")
+    parser.add_argument("--output_dir", type=str, default="output", help="Directory to store results. Relative to script directory.")
     parser.add_argument("--num_proc", type=int, default=1, help="Number of parallel threads")
     parser.add_argument("--push_to_hub", action='store_true', help="Whether to push results to Hugging Face Hub")
     parser.add_argument("--tmp_dir", type=str, default="tmp", help="Temporary directory for caching and repos. Relative to script directory.")
